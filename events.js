6d090b0cd851c08adb46b1237ee2c7da111da623
Padmanabhan <padmanabhansridhar2005@gmail.com>
2025-10-29 18:44:08 +0530

    final pass types

--- events.js
+++ events.js
@@ -1,17 +1,18 @@
 export const passTypeToEvents = {
   AAA: ["1", "2", "3", "4"],
   AA: ["1", "2", "3", "4"],
+  AAG: ["1", "2", "3", "4"],
   OAA: ["1", "2", "3", "4"],
-  D1: ["1"],
+  D1: ["1","4"],
   D2: ["2"],
   D3: ["3"],
-  D1C : ["1", "4"],
-  D12 : ["1", "2"],
-  D13 : ["1", "3"],
+  D12 : ["1", "2","4"],
+  D13 : ["1", "3","4"],
   D23 : ["2", "3"],
-  C : ["4"],
   E : [],
   FACULTY: ["1", "2", "3", "4"],
+  PHD : ["1", "2", "3", "4"],
+  SR : ["1"]
 };
 
 const events = Object.entries(passTypeToEvents).reduce((acc, [passType, eventList]) => {
